eaba74bf868ca39eb364f4420077941f4c404385
Daniel Miedema <danielmiedema@me.com>
2013-04-26 14:50:48 -0700

    Initial Commit

---
+++ Application/DMViewController.h
@@ -0,0 +1,13 @@
+//
+//  DMViewController.h
+//  Application
+//
+//  Created by Daniel Miedema on 4/26/13.
+//  Copyright (c) 2013 Daniel Miedema. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DMViewController : UIViewController
+
+@end
---
+++ Application/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Application
+//
+//  Created by Daniel Miedema on 4/26/13.
+//  Copyright (c) 2013 Daniel Miedema. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DMAppDelegate class]));
+    }
+}
