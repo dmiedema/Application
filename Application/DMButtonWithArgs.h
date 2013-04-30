97fe77726327f7a6aca5e89930221dbab9a4cbad
Daniel Miedema <danielmiedema@me.com>
2013-04-30 00:21:21 -0700

    Extending UIButton so I can send params

---
+++ Application/DMButtonWithArgs.h
@@ -0,0 +1,15 @@
+//
+//  DMButtonWithArgs.h
+//  Application
+//
+//  Created by Daniel Miedema on 4/29/13.
+//  Copyright (c) 2013 Daniel Miedema. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DMButtonWithArgs : UIButton
+
+@property (nonatomic, strong) NSString *link;
+
+@end
---
+++ Application/DMButtonWithArgs.m
@@ -0,0 +1,31 @@
+//
+//  DMButtonWithArgs.m
+//  Application
+//
+//  Created by Daniel Miedema on 4/29/13.
+//  Copyright (c) 2013 Daniel Miedema. All rights reserved.
+//
+
+#import "DMButtonWithArgs.h"
+
+@implementation DMButtonWithArgs
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
